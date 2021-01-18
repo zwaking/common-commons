89e23a8272db33a09cfe20971c2ef5414244a05e
zwaking <bleach2625@126.com>
2021-01-18 20:20:07 +0800

    JsonUtils添加toJsonNode、toMap方法

--- src/main/java/com/zwaking/common/utils/JsonUtils.java
+++ src/main/java/com/zwaking/common/utils/JsonUtils.java
@@ -1,6 +1,9 @@
 package com.zwaking.common.utils;
 
+import java.util.Map;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
@@ -24,6 +27,39 @@ public class JsonUtils {
         return null;
     }
 
+    public static Map bean2Map(Object obj) {
+        try {
+            if (String.class.isAssignableFrom(obj.getClass())) {
+                return objectMapper.readValue(obj.toString(), Map.class);
+            }
+            return objectMapper.convertValue(obj, Map.class);
+        } catch (Exception e) {
+            log.debug("bean2Map Convert Has Error", e);
+        }
+        return null;
+    }
+
+    public static JsonNode bean2JsonNode(Object obj) {
+        try {
+            if (String.class.isAssignableFrom(obj.getClass())) {
+                return objectMapper.readTree(obj.toString());
+            }
+            return objectMapper.readTree(objectMapper.writeValueAsBytes(obj));
+        } catch (Exception e) {
+            log.debug("bean2JsonNode Convert Has Error", e);
+        }
+        return null;
+    }
+
+    /**
+     * <p>
+     * 将在下个下版本移除
+     * </p>
+     * 
+     * @see com.zwaking.common.utils.JsonUtils#bean2JsonNode(Object)
+     * @return
+     */
+    @Deprecated
     public static ObjectNode bean2Json(Object obj) {
         return objectMapper.convertValue(obj, ObjectNode.class);
     }
--- src/test/java/com/zwaking/common/utils/TestJsonUtils.java
+++ src/test/java/com/zwaking/common/utils/TestJsonUtils.java
@@ -1,22 +1,64 @@
 package com.zwaking.common.utils;
 
-import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import org.junit.jupiter.api.Test;
 
 public class TestJsonUtils {
 
     @Test
-    public void testBean2Json(){
+    public void testBean2Json() {
         List<TTT> tttList = new ArrayList<>();
         TTT ttt1 = new TTT();
+        ttt1.setName("ttt1");
         TTT ttt2 = new TTT();
         TTT ttt3 = new TTT();
         tttList.add(ttt1);
         tttList.add(ttt2);
         tttList.add(ttt3);
-        System.out.println(JsonUtils.bean2Json(tttList));
+        System.out.println(JsonUtils.bean2Json(ttt1).get("name"));
+    }
+
+    @Test
+    public void testBean2Map() {
+        TTT ttt = new TTT();
+        ttt.setName("ttt1");
+        ttt.setAge(1L);
+        ttt.setSex("1");
+        System.out.println(JsonUtils.bean2Map(ttt));
+        String jsonStr = JsonUtils.bean2JsonStr(ttt);
+        System.out.println(JsonUtils.bean2Map(jsonStr));
+    }
+
+    @Test
+    public void testBean2JsonNode() {
+         List<TTT> tttList = new ArrayList<>();
+         TTT ttt1 = new TTT();
+         ttt1.setName("ttt1");
+         ttt1.setAge(1L);
+         ttt1.setSex("1");
+         TTT ttt2 = new TTT();
+         ttt2.setName("ttt2");
+         ttt2.setAge(2L);
+         ttt2.setSex("2");
+         TTT ttt3 = new TTT();
+         ttt3.setName("ttt3");
+         ttt3.setAge(3L);
+         ttt3.setSex("3");
+         tttList.add(ttt1);
+         tttList.add(ttt2);
+         tttList.add(ttt3);
+         System.out.println(JsonUtils.bean2JsonNode(tttList));
+
+        TTT ttt = new TTT();
+        ttt.setName("ttt1");
+        ttt.setAge(1L);
+        ttt.setSex("1");
+        String jsonStr = JsonUtils.bean2JsonStr(ttt);
+        System.out.println(JsonUtils.bean2JsonNode(jsonStr).get("name"));
+
+        System.out.println(JsonUtils.bean2JsonNode(1).asText());
     }
 
 }
